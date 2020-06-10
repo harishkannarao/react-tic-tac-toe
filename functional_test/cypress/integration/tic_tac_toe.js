3b5c5539c76b8b223e54662e8543304944255020
harishkannarao <harish.gtec@gmail.com>
2020-06-10 19:00:37 +0530

    Adding additional test case

--- functional_test/cypress/integration/tic_tac_toe.js
+++ functional_test/cypress/integration/tic_tac_toe.js
@@ -15,6 +15,16 @@ describe('Play tic tac toe', () => {
                 expect($div).to.have.length(1)
                 expect($div[0]).to.have.text('Winner: X')
             });
+    });
+
+    it('Displays the moves in sequence', () => {
+        cy.visit('http://localhost:3000');
+
+        cy.get('[data-testid="status"')
+            .eq(0)
+            .then(($button) => {
+                expect($button.text()).to.equal('Next player: X');
+            });
 
         cy.get('[data-testid="move"')
             .eq(0)
@@ -23,11 +33,32 @@ describe('Play tic tac toe', () => {
                 expect(move).to.equal('Go to game start');
             });
 
+        const squareId = '[data-testid="square"]';
+
+        cy.get(squareId).eq(0).click();
+        cy.get('[data-testid="status"')
+            .eq(0)
+            .then(($button) => {
+                expect($button.text()).to.equal('Next player: O');
+            });
         cy.get('[data-testid="move"')
             .eq(1)
             .then(($button) => {
                 const move = $button.text();
                 expect(move).to.equal('Go to move #1');
+            });
+
+        cy.get(squareId).eq(1).click();
+        cy.get('[data-testid="status"')
+            .eq(0)
+            .then(($button) => {
+                expect($button.text()).to.equal('Next player: X');
+            });
+        cy.get('[data-testid="move"')
+            .eq(2)
+            .then(($button) => {
+                const move = $button.text();
+                expect(move).to.equal('Go to move #2');
             });
     });
 });
