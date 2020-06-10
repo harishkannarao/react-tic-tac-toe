0acc23ee4f0e40b99f75b3859cda144bce913be7
harishkannarao <harish.gtec@gmail.com>
2020-06-10 13:23:06 +0530

    Enhancing cypress with then

--- functional_test/cypress/integration/tic_tac_toe.js
+++ functional_test/cypress/integration/tic_tac_toe.js
@@ -15,5 +15,19 @@ describe('Play tic tac toe', () => {
                 expect($div).to.have.length(1)
                 expect($div[0]).to.have.text('Winner: X')
             });
+
+        cy.get('[data-testid="move"')
+            .eq(0)
+            .then(($button) => {
+                const move = $button.text();
+                expect(move).to.equal('Go to game start');
+            });
+
+        cy.get('[data-testid="move"')
+            .eq(1)
+            .then(($button) => {
+                const move = $button.text();
+                expect(move).to.equal('Go to move #1');
+            });
     });
 });
